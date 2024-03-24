518dd331066e6dd40a35252136671440b1dfb68b
Shifyannn <asifyan01@gmail.com>
2024-03-24 15:47:24 +0700

    Penambahan content table

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
 import AddItem from './components/addItem';
 import Navbar from './components/navbar';
+import Content from './components/content';
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="main my-10">
-        <AddItem />
+      <div className="main my-10 mx-5">
+        <div className="my-5">
+          <AddItem />
+        </div>
+        <div className="my-5">
+          <Content />
+        </div>
       </div>
     </>
   );
